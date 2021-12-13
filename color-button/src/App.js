2dafc8c625a72ccd245ca7959754e54d13e9f922
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-12-13 14:23:35 -0300

    Added test on button color

--- color-button/src/App.js
+++ color-button/src/App.js
@@ -1,10 +1,27 @@
+import { useState } from 'react';
+
 import './App.css';
 
 function App() {
+  const [buttonColor, setbuttonColor] = useState('red');
+
+  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+
+  const buttonColorHandler = () => {
+    setbuttonColor((prev) => {
+      return prev === 'red' ? 'blue' : 'red';
+    });
+  };
+
   return (
     <div className="App">
       <div>
-        <button style={{backgroundColor: 'red'}}>Change to blue</button>
+        <button
+          style={{ backgroundColor: buttonColor }}
+          onClick={buttonColorHandler}
+        >
+          Change to {newButtonColor}
+        </button>
       </div>
     </div>
   );
--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,14 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('button has initial color', () => {
+  // GIVEN
   render(<App />);
 
+  // WHEN
   const colorButton = screen.getByRole('button', { name: 'Change to blue' });
 
+  //THEN
   expect(colorButton).toHaveStyle({backgroundColor: 'red'});
+
+  fireEvent.click(colorButton);
+
+  expect(colorButton).toHaveStyle({backgroundColor: 'blue'});
+  expect(colorButton.textContent).toBe('Change to red');
 });
-
-test('button turns blue when clicked', () => {
-
-})
