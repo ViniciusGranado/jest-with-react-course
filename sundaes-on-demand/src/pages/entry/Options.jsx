080edae267d64b61517623a7876a6dccdc8317b4
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-01-04 19:23:11 -0300

    Added code to make Options tests valid

--- sundaes-on-demand/src/pages/entry/Options.jsx
+++ sundaes-on-demand/src/pages/entry/Options.jsx
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Row from 'react-bootstrap/Row';
+import { ScoopOptions } from './ScoopOptions';
 
-export const Options = ({optionType}) => {
-  return <div></div>
-}
+export const Options = ({ optionType }) => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => {
+        // TODO
+      });
+  }, [optionType]);
+
+  const ItemComponent = optionType === 'scoops' ? ScoopOptions : null;
+
+  const optionItens = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItens}</Row>;
+};
--- sundaes-on-demand/src/pages/entry/tests/Options.test.js
+++ sundaes-on-demand/src/pages/entry/tests/Options.test.js
@@ -1,14 +1,14 @@
-import {render, screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Options } from '../Options';
 
-test('displays image for each scoop option from server', () => {
-  render(<Options optionType="scoops"/>)
+test('displays image for each scoop option from server', async () => {
+  render(<Options optionType="scoops" />);
 
-  const scoopImages = screen.getAllByRole('img', {name: /scoop$/i});
+  const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
 
   expect(scoopImages).toHaveLength(2);
 
   const altTex = scoopImages.map((element) => element.alt);
 
   expect(altTex).toEqual(['Chocolate scoop', 'Vanilla scoop']);
-})
+});
