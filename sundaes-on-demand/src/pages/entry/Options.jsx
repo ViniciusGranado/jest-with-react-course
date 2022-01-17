14a409335c81909ab4136367b2f59c0de0525762
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-01-17 19:46:22 -0300

    Added OrderEntry tests

--- sundaes-on-demand/src/pages/entry/Options.jsx
+++ sundaes-on-demand/src/pages/entry/Options.jsx
@@ -3,18 +3,22 @@ import axios from 'axios';
 import Row from 'react-bootstrap/Row';
 import { ScoopOptions } from './ScoopOptions';
 import { ToppingOptions } from './ToppingOptions';
+import { AlertBanner } from '../common/AlertBanner';
 
 export const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     axios
       .get(`http://localhost:3000/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((error) => {
-        // TODO
-      });
+      .catch((error) => setError(true));
   }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   const ItemComponent = optionType === 'scoops' ? ScoopOptions : ToppingOptions;
 
