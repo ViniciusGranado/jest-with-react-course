04f8d53a438f4e01e2c963c04df912522ab1af17
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-01-20 18:51:20 -0300

    Added code to make tests pass

--- sundaes-on-demand/src/pages/entry/ScoopOptions.jsx
+++ sundaes-on-demand/src/pages/entry/ScoopOptions.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
 
-export const ScoopOptions = ({ name, imagePath }) => {
+export const ScoopOptions = ({ name, imagePath, updateItemCount }) => {
+  const handleChange = (event) => {
+    updateItemCount(name, event.target.value);
+  }
+
   return (
     <Col sx={12} sm={6} md={4} ls={3} style={{ textAlign: 'center' }}>
       <img
@@ -9,6 +15,23 @@ export const ScoopOptions = ({ name, imagePath }) => {
         src={`http://localhost:3000/${imagePath}`}
         alt={`${name} scoop`}
       />
+
+      <Form.Group
+        controlId={`${name}-count`}
+        as={Row}
+        style={{ marginTop: '10px' }}
+      >
+        <Form.Label column sx="6" style={{ textAlign: 'right' }}>
+          {name}
+        </Form.Label>
+        <Col xs="5" style={{ textAlign: 'left' }}>
+          <Form.Control
+            type="number"
+            defaultValue={0}
+            onChange={handleChange}
+          />
+        </Col>
+      </Form.Group>
     </Col>
   );
 };
