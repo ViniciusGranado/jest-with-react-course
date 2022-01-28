1f2e2dccab9c4781839140a75f2ffcf3335a9f9c
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-01-28 14:54:47 -0300

    Added gran total tests

--- sundaes-on-demand/src/pages/entry/OrderEntry.jsx
+++ sundaes-on-demand/src/pages/entry/OrderEntry.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { useOrderDetails } from '../../contexts/OrderDetail';
 
 import { Options } from './Options';
 
 export const OrderEntry = () => {
+  const [ orderDetails ] = useOrderDetails();
+
   return (
     <div>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
+      <h2>Grand total: {orderDetails.totals.grandTotal}</h2>
     </div>
   );
 };
